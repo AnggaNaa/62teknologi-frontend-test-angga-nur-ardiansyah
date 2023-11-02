c0ffd088ff26d8aa4826aa18ad704057ea2efb4d
AnggaNaa <angga.ardiansyah056@gmail.com>
2023-11-02 12:25:37 +0700

    fix: code

--- FE/src/lib/api.js
+++ FE/src/lib/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:3000/api/yelp/businesses",
+  baseURL:
+    "https://62teknologi-frontend-test-angga-nur-ardiansyah-6i0pqeya1.vercel.app/api/yelp/businesses",
   headers: {
     Authorization: import.meta.env.VITE_API_KEY,
   },
